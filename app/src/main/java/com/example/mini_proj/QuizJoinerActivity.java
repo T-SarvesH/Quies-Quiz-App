48e10c9858bb2e18c123e15aa25d6f213a788cf3
T-SarvesH <141503222+T-SarvesH@users.noreply.github.com>
2024-05-03 19:24:10 +0530

    Main quiz

--- app/src/main/java/com/example/mini_proj/QuizJoinerActivity.java
+++ app/src/main/java/com/example/mini_proj/QuizJoinerActivity.java
@@ -1,5 +1,8 @@
 package com.example.mini_proj;
 
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -7,6 +10,8 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.core.view.WindowCompat;
 import androidx.navigation.NavController;
@@ -16,11 +21,26 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.mini_proj.databinding.ActivityQuizJoinerBinding;
 
-public class QuizJoinerActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class QuizJoinerActivity extends AppCompatActivity implements View.OnClickListener {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityQuizJoinerBinding binding;
     public static String resu;
+
+    TextView totalQuestionsTextView;
+    TextView questionTextView;
+    Button ansA, ansB, ansC, ansD;
+    Button submitBtn;
+
+    int score=0;
+    int totalQuestion = QuestionAnswer.question.size();
+    int currentQuestionIndex = 0;
+    int selectedAnswer = 0;
+    ArrayList<Integer> map =  new ArrayList<Integer>();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,14 +49,100 @@ public class QuizJoinerActivity extends AppCompatActivity {
         binding = ActivityQuizJoinerBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        map.add(R.id.ans_A);
+        map.add(R.id.ans_B);
+        map.add(R.id.ans_C);
+        map.add(R.id.ans_D);
+
+        totalQuestionsTextView = findViewById(R.id.total_question);
+        questionTextView = findViewById(R.id.question);
+        ansA = findViewById(R.id.ans_A);
+        ansB = findViewById(R.id.ans_B);
+        ansC = findViewById(R.id.ans_C);
+        ansD = findViewById(R.id.ans_D);
+        submitBtn = findViewById(R.id.submit_btn);
+
+        ansA.setOnClickListener(this);
+        ansB.setOnClickListener(this);
+        ansC.setOnClickListener(this);
+        ansD.setOnClickListener(this);
+        submitBtn.setOnClickListener(this);
+
+        totalQuestionsTextView.setText("Total questions : "+totalQuestion);
+
+        loadNewQuestion();
+
         //setSupportActionBar(binding.toolbar);
 
         //NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_quiz_joiner);
         //appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+    }
+    @Override
+    public void onClick(View view) {
+
+        ansA.setBackgroundColor(Color.WHITE);
+        ansB.setBackgroundColor(Color.WHITE);
+        ansC.setBackgroundColor(Color.WHITE);
+        ansD.setBackgroundColor(Color.WHITE);
+
+        Button clickedButton = (Button) view;
+        if(clickedButton.getId()==R.id.submit_btn){
+            if(selectedAnswer+1==(QuestionAnswer.correctAnswers.get(currentQuestionIndex))){
+                score++;
+            }
+            currentQuestionIndex++;
+            loadNewQuestion();
+
+
+        }else {
+            selectedAnswer = map.indexOf(clickedButton.getId());
+            clickedButton.setBackgroundColor(Color.MAGENTA);
+        }
+
+    }
+
+    void loadNewQuestion(){
+
+        if(currentQuestionIndex == totalQuestion ){
+            finishQuiz();
+            return;
+        }
+
+        questionTextView.setText(QuestionAnswer.question.get(currentQuestionIndex));
+        ansA.setText((CharSequence) QuestionAnswer.choices.get(currentQuestionIndex).get(0));
+        ansB.setText((CharSequence) QuestionAnswer.choices.get(currentQuestionIndex).get(1));
+        ansC.setText((CharSequence) QuestionAnswer.choices.get(currentQuestionIndex).get(2));
+        ansD.setText((CharSequence) QuestionAnswer.choices.get(currentQuestionIndex).get(3));
+
+    }
+
+    void finishQuiz(){
+        String passStatus = "";
+        if(score > totalQuestion*0.60){
+            passStatus = "Passed";
+        }else{
+            passStatus = "Failed";
+        }
+
+        new AlertDialog.Builder(this)
+                .setTitle(passStatus)
+                .setMessage("Score is "+ score+" out of "+ totalQuestion)
+                .setPositiveButton("Go to Home Page",(dialogInterface, i) -> restartQuiz() )
+                .setCancelable(false)
+                .show();
 
 
     }
+    void restartQuiz(){
+        score = 0;
+        currentQuestionIndex =0;
+        Intent i = new Intent(QuizJoinerActivity.this,MainActivity.class);
+        startActivity(i);
+    }
+
+
+
     /*
     @Override
     public boolean onSupportNavigateUp() {
@@ -44,4 +150,6 @@ public class QuizJoinerActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
     }*/
+
+
 }
